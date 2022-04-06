ed3c27d28e2c1413e8c060b791710396d4bc02df
susara13 <86735022+susara13@users.noreply.github.com>
2022-04-06 20:48:50 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.header("Hello World")
 st.write("This is my first streamlit web app!")
+
+st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+show = st.checkbox('I agree the terms and conditions')
+if show:
+    st.write(pd.DataFrame({
+    'Intplan': ['yes', 'yes', 'yes', 'no'],
+    'Churn Status': [0, 0, 0, 1]
+}))
